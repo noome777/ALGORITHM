c000cde636322d09b1f86391b04f3e07a3643244
AREUM\lee99 <lee990103@gmail.com>
2022-09-15 18:59:43 +0900

    짝수와 홀수

---
+++ programmers_level1/src/programmers_level1/Solution01.java
@@ -0,0 +1,14 @@
+package programmers_level1;
+
+public class Solution01 {
+
+	class Solution {
+	    public String solution(int num) {
+	       if(num%2 == 0){
+	           return "Even";
+	       } else {
+	           return "Odd";
+	       }
+	    }
+	}
+}
