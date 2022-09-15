c55609b4d84349e93ee83f9a9bd5fd28ef01a937
AREUM\lee99 <lee990103@gmail.com>
2022-09-15 21:16:56 +0900

    약수의 합

--- programmers_level1/src/programmers_level1/Main.java
+++ programmers_level1/src/programmers_level1/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Solution02 s = new Solution02();
-		s.solution(356);
+		Solution03 s = new Solution03();
+		s.solution(6);
 
 	}
 
